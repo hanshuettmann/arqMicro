f83e80b3465ffacc291d8aa6b40ad94bd17d277c
Hans <hans.huttmann@gmail.com>
2022-05-30 23:26:37 -0300

    add assembly functions

--- STM32Workspace/adm_c16/Core/Inc/asm_func.h
+++ STM32Workspace/adm_c16/Core/Inc/asm_func.h
@@ -2,6 +2,12 @@
 
 #include <stdint.h>
 
-
 void asm_svc (void);
 uint32_t asm_sum (uint32_t firstOperand, uint32_t secondOperand);
+void asm_zeros (uint32_t *vector, uint32_t longitud);
+void asm_scalar32(uint32_t *vectorIn, uint32_t *vectorOut, uint32_t length,
+		uint32_t scalar);
+void asm_scalar16(uint16_t *vectorIn, uint16_t *vectorOut, uint16_t length,
+		uint16_t scalar);
+void asm_scalar12(uint16_t *vectorIn, uint16_t *vectorOut, uint16_t length,
+		uint16_t scalar);
